7187f1d73ed44e1d5e7923e0b40911c1d24d421e
Mariana Ibarra <marian4ibarr4@gmail.com>
2022-04-02 09:12:56 -0500

    to cloud

--- js/collisions.js
+++ js/collisions.js
@@ -1,6 +1,10 @@
 let canvas = document.querySelector("canvas"),
-  context = canvas.getContext("2d"),
-  colorPallet = [
+  context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let colorPallet = [
     "251, 248, 204",
     "253, 228, 207",
     "255, 207, 210",
@@ -13,15 +17,9 @@ let canvas = document.querySelector("canvas"),
     "185, 251, 192",
   ],
   mousePos = {
-    x: undefined,
-    y: undefined,
-  },
-  gravity = 0.9,
-  friction = 0.99,
-  minCloseness = 50;
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+  };
 
 window.addEventListener("mousemove", (event) => {
   mousePos.x = event.clientX;
@@ -38,8 +36,8 @@ class Ball {
   constructor(x, y, dx, dy, radius, color) {
     this.x = x;
     this.y = y;
-    this.dy = dy;
     this.dx = dx;
+    this.dy = dy;
     this.radius = radius;
     this.color = color;
   }
@@ -47,32 +45,14 @@ class Ball {
   draw() {
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    context.strokeStyle = `rgb(${this.color})`;
     context.fillStyle = `rgba(${this.color},0.3)`;
     context.fill();
+    context.stroke();
     context.closePath();
   }
 
   update() {
-    if (this.y + this.radius + this.dy > canvas.height) {
-      this.dy = -this.dy * friction;
-    } else {
-      this.dy += gravity;
-    }
-
-    // if (
-    //   mousePos.x - this.x < minCloseness &&
-    //   mousePos.x - this.x > -minCloseness &&
-    //   mousePos.y - this.y < minCloseness &&
-    //   mousePos.y - this.y > -minCloseness
-    // ) {
-    if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
-      this.x -= this.dx;
-    }
-    //   this.x += this.dx;
-    // }
-
-    this.y += this.dy;
-    this.x;
     this.draw();
   }
 }
@@ -83,41 +63,61 @@ const randomColor = () =>
 const randomIntFromRange = (min, max) =>
   Math.round(Math.random() * (max - min + 1) + min);
 
-const distance = (x1, x2, y1, y2) => {
-  const xDist = x1 - x2;
-  const yDist = y1 - y2;
+const distance = (el1, el2) => {
+  const xDist = el1.x + el1.radius - (el2.x + el1.radius);
+  const yDist = el1.y + el1.radius - (el2.y + el1.radius);
+
+  //   // Better performance algo
+  //   return (
+  //     el1.x * el1.x +
+  //     2 * el2.x * el2.radius +
+  //     el2.radius * el2.radius
+  //   );
+
+  //   return {
+  //     y1: el1.y,
+  //     x1: el1.x,
+  //     x2: el2.x,
+  //     y2: el2.y,
+  //     radius1: el1.radius,
+  //     radius2: el2.radius,
+  //   };
 
   return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
 };
 
-let ballArray = new Array();
+let objects = new Array();
+let ball1, ball2;
 
 const initObjects = () => {
-  ballArray = [];
-  for (let index = 0; index < 50; index++) {
-    let radius = randomIntFromRange(10, 30);
-    ballArray.push(
-      new Ball(
-        randomIntFromRange(radius, canvas.width - radius),
-        randomIntFromRange(0, canvas.height - radius),
-        randomIntFromRange(-2, 2),
-        5,
-        radius,
-        randomColor()
-      )
-    );
+  objects = [];
+  ball1 = new Ball(200, 200, 0, 0, 30, randomColor());
+  ball2 = new Ball(
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    30,
+    randomColor()
+  );
+  for (let index = 0; index < 400; index++) {
+    // Do something
+    // objects.push()
   }
 };
 
 const animate = () => {
   requestAnimationFrame(animate);
-
   context.clearRect(0, 0, innerWidth, innerHeight);
+  ball1.update();
+  ball2.x = mousePos.x;
+  ball2.y = mousePos.y;
+  ball2.update();
 
-  for (let index = 0; index < ballArray.length; index++) {
-    ballArray[index].update();
+  if (distance(ball1, ball2) < ball1.radius * 2) {
+    ball1.x += 1;
   }
 };
 
-animate();
 initObjects();
+animate();
